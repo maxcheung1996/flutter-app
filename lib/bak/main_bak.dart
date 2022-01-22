19aa440ad4d3c2634801d4f5f58cf745e72c5e48
maxcheung1996 <cheungwaiman1919@gmail.com>
2022-01-23 00:21:54 +0800

    23012022_0021

---
+++ lib/bak/main_bak.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.cyan[50],
+        title: const Text(
+          'My First App',
+          style: TextStyle(
+              color: Colors.purple,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              fontSize: 25.0,
+              fontFamily: 'IndieFlower-Regular'),
+        ),
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            child: Image.asset('assets/Seaboat.jpg'),
+            flex: 3,
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.cyan,
+              child: const Text('1'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.pinkAccent,
+              child: const Text('2'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text('3'),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: const Text('click'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MaterialApp(
-    home: Home(),
-  ));
-}
+void main() => runApp(MaterialApp(home:))
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.cyan[50],
-        title: const Text(
-          'My First App',
-          style: TextStyle(
-              color: Colors.purple,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              fontSize: 25.0,
-              fontFamily: 'IndieFlower-Regular'),
-        ),
-      ),
-      body: Center(
-          child: ElevatedButton(
-        onPressed: () => {},
-        child: const Text('Click Me'),
-        style: ElevatedButton.styleFrom(primary: Colors.lightBlue),
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
-        child: const Text('click'),
-      ),
-    );
-  }
-}
